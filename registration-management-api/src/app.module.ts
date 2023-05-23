b0d9ca92193bbd9b5e3296d9729c6ed19bafb477
Melvin <Melvingiebels@gmail.com>
2023-05-23 11:18:20 +0200

    Add students module

--- registration-management-api/src/app.module.ts
+++ registration-management-api/src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { StudentsModule } from './students/students.module';
 
 @Module({
-  imports: [],
+  imports: [StudentsModule],
   controllers: [AppController],
   providers: [AppService],
 })
