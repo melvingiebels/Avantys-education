c79a8f7490769b88a055a8769943a0d363ae4232
NoahKeijzer <noah.cristiaan@gmail.com>
2023-05-26 13:30:32 +0200

    EF database working

--- TestManagement/TestManagement/Program.cs
+++ TestManagement/TestManagement/Program.cs
@@ -10,9 +10,13 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddDbContext<TestManagementDbContext>(
-    options => options.UseSqlServer(builder.Configuration.GetConnectionString("SQL")));
+builder.Services.AddDbContext<TestManagementDbContext>(options =>
+{
+    var connectionString = builder.Configuration.GetConnectionString("SQL");
+    options.UseSqlServer(connectionString,
+        b => b.MigrationsAssembly("TestManagement"));
 
+});
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
