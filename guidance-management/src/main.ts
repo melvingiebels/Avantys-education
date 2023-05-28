a745d91b39fbd4faba5cdae7b26fb41bbdde0466
Luuk Bartels <l.bartes1@student.avans.nl>
2023-05-28 13:48:13 +0200

    last update guidancemanagement

--- guidance-management/src/main.ts
+++ guidance-management/src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { MicroserviceOptions, Transport } from '@nestjs/microservices';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   let app = await NestFactory.create(AppModule);
@@ -14,6 +15,16 @@ async function bootstrap() {
       }
     }
   })
+  const config = new DocumentBuilder()
+    .setTitle('Guidance Management')
+    .setDescription('The Guidance Management service API endpoints')
+    .setVersion('1.0')
+    .addTag('Guidance')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+
   await app.startAllMicroservices();
   await app.listen(3000);
 }
