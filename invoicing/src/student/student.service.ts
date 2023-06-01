02c8ceb9c7d353175edf9cf228e0fda4dccf3179
Melvin <Melvingiebels@gmail.com>
2023-06-01 11:07:34 +0200

    Fixed Invoice

--- invoicing/src/student/student.service.ts
+++ invoicing/src/student/student.service.ts
@@ -10,38 +10,50 @@ import { Balance } from 'src/domain/balance';
 @Injectable()
 export class StudentService {
   constructor(
-    @InjectRepository(Student) private studentRepository: Repository<Student>, @InjectRepository(Invoice) private invoiceRepository: Repository<Invoice>
-    ) { }
-    
-    async createStudent(student: Student): Promise<Student> {
-      return this.studentRepository.save(student);
-    }
-    async calculateTotalBalance(studentId: number): Promise<Balance> {
-      const balance = new Balance();
-      
-      balance.student = await this.studentRepository.findOneBy({
-        id: studentId
-      });
-      
-      const invoiceEvents = await this.invoiceRepository.findBy({ student: { id: balance.student.id } });
-      
-      balance.totalInvoiceAmount = 0;
-      
-      invoiceEvents.forEach((invoiceEvent) => {
-        balance.totalInvoiceAmount += invoiceEvent.invoiceAmount;
-      });
-      
-      return balance;
-    }
-    async setStudentAcceptance(studentId: number, acceptance: boolean) {
-      const student = await this.studentRepository.findOneBy({
-        id: studentId
-      });
+    @Inject('REGISTRATION_SERVICE') private client: ClientProxy,
+    @InjectRepository(Student) private studentRepository: Repository<Student>,
+    @InjectRepository(Invoice) private invoiceRepository: Repository<Invoice>,
+  ) {}
 
-   
-      student.authorizedPayments = acceptance;
-
-      return this.studentRepository.save(student);
-    }
+  async createStudent(student: Student): Promise<Student> {
+    return this.studentRepository.save(student);
   }
-  
+  async calculateTotalBalance(studentId: number): Promise<Balance> {
+    const balance = new Balance();
+
+    balance.student = await this.studentRepository.findOneBy({
+      id: studentId,
+    });
+
+    const invoiceEvents = await this.invoiceRepository.findBy({
+      student: { id: balance.student.id },
+    });
+
+    balance.totalInvoiceAmount = 0;
+
+    invoiceEvents.forEach((invoiceEvent) => {
+      balance.totalInvoiceAmount += invoiceEvent.invoiceAmount;
+    });
+
+    return balance;
+  }
+  async setStudentAcceptance(studentId: number, acceptance: boolean) {
+    const student = await this.studentRepository.findOneBy({
+      id: studentId,
+    });
+
+    console.log(student);
+
+    student.authorizedPayments = acceptance;
+
+    // if false, send an event to registrationmanagement
+    if (!acceptance) {
+      this.client.emit('StudentPaymentDeclined', studentId);
+    } else {
+      this.client.emit('StudentPaymentAccepted', studentId);
+    }
+
+    console.log(student);
+    return await this.studentRepository.update(studentId, student);
+  }
+}
